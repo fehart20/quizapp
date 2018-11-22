261abe448b048f8aaaabf990ec02967768747f7e
Felix Hartnagel <45048295+fehart20@users.noreply.github.com>
2018-11-22 17:32:04 +0100

    Update database.php

--- database.php
+++ database.php
@@ -14,7 +14,7 @@ $mysqli = new mysqli($db_host, $db_user, $db_pass, $db_name);
 //Error Handler
 
 if($mysqli->connect_error) {
-	printf("Verbindung fehlgeschlagen: %s\n", $mysqli->conncet_error);
+	printf("Connection failed: %s\n", $mysqli->connect_error);
 	exit();
 }
 ?>
