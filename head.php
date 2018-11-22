dcb65327464487ca46b50b3897d9616b6e5abc9f
Felix Hartnagel <45048295+fehart20@users.noreply.github.com>
2018-11-22 17:37:34 +0100

    Update head.php

--- head.php
+++ head.php
@@ -3,14 +3,14 @@
 <html>
 	<head>
 		<meta charset="utf-8" />
-		<title>Technik Quiz</title>
+		<title>Quizapp</title>
 		<link rel="stylesheet" href="css/style.css" type="text/css" />
 		<link href="https://fonts.googleapis.com/css?family=Poppins" rel="stylesheet">
 	</head>
 </body>
 	<header>
 		<div class="container">
-			<h1>Technik Quiz</h1>
+			<h1>Quizapp</h1>
 		</div>
 	</header>
 </html>
