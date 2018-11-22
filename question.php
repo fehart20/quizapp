79d316ed5ee74f452254bdf78eebcd4f12ee475f
Felix Hartnagel <45048295+fehart20@users.noreply.github.com>
2018-11-22 17:43:16 +0100

    Update question.php

--- question.php
+++ question.php
@@ -49,7 +49,7 @@
 <body>
 	<main>
 		<div class="container">
-			<div class="current">Frage <?php echo $question['question_number']; ?> von <?php echo $total; ?></div>
+			<div class="current">Question <?php echo $question['question_number']; ?> of <?php echo $total; ?></div>
 			<p class="question">
 				<?php echo $question['text'];?>
 			</p>
@@ -59,7 +59,7 @@
 						<li><input name="choice" type="radio" value="<?php echo $row['id']; ?>" /><?php echo $row['text']; ?></li>
 					<?php endwhile; ?>
 				</ul>
-				<input type="submit" value="Absenden" />
+				<input type="submit" value="Submit" />
 				<input type="hidden"  name="number" value="<?php echo $number; ?>" />
 			</form>
 		</div>
